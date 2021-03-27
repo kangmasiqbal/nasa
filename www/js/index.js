daa38f85b0e2ced13a671b5004dd9942db18e212
Mokhammad Iqbal <kangmas.iqbal@gmail.com>
2021-03-27 19:48:08 +0700

    add API key NASA

--- www/js/index.js
+++ www/js/index.js
@@ -3,7 +3,7 @@ var app = {
     // Application Constructor
     initialize: function() {
     
-        var url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY";
+        var url = "https://api.nasa.gov/planetary/apod?api_key=fsmAEo2K4fcaHCZ1YPDRHNAlGwRx0qVZmqdu5XWg";
         
         $("#loadImage").click(function(){
             $.ajax({
